b242c8eecd6b0c56b11277e6441519a926255cc7
tawatchai-yuth <tawatchai_yuth@hotmail.com>
2021-02-17 09:03:25 +0700

    update nuxt

---
+++ pages/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <b-container class="mt-5">
+    <div>
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group id="input-group-1" label="Title:" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="form.title"
+            type="text"
+            placeholder="Enter title"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-2"
+          label="Description:"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="form.description"
+            placeholder="Description"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-3"
+          label="อัพโหลดรูปภาพ:"
+          label-for="input-3"
+        >
+          <b-form-input
+            id="input-3"
+            v-model="form.upImage"
+            type="text"
+            placeholder="อัพโหลดรูปภาพ"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+      <b-card class="mt-3" header="Form Data Result">
+        <pre class="m-0">{{ form }}</pre>
+      </b-card>
+    </div>
+  </b-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          title: '',
+          description: '',
+          upImage: '',
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.description = ''
+        this.form.upImage = ''
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
