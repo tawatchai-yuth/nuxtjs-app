6aada68cf9ea3e939878dc10d223e89aa43fbd7c
tawatchai-yuth <tawatchai_yuth@hotmail.com>
2021-02-19 22:12:08 +0700

    update auth register

--- api/model/order.js
+++ api/model/order.js
@@ -4,6 +4,6 @@ const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', require: true },
     quantity: { type: Number, default: 1 }
-})
+});
 
 module.exports = mongoose.model('Order', orderSchema);
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRouters = require('./api/routes/orders');
+const userRouters = require('./api/routes/users');
 
 mongoose.connect("mongodb+srv://node-shop:" + process.env.MONGO_ATLAS_PW +
     "@node-rest-shop.4ggjs.mongodb.net/db_nuxtjs?retryWrites=true&w=majority", {
@@ -43,6 +44,7 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRouters);
+app.use('/users', userRouters);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
