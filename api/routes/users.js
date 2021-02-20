39ba7cf7df6e145bbe2fc9ce221c5d9081dc44a3
tawatchai-yuth <tawatchai_yuth@hotmail.com>
2021-02-20 22:42:52 +0700

    update api

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,64 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const checkAuth = require('../middleware/check-auth');
 
-const User = require('../model/user');
+const UserController = require('../controllers/users');
 
-router.post('/signup', (req, res, next) => {
-    User.find({ email: req.body.email })
-        .exec()
-        .then(user => {
-            if (user.length >= 1) {
-                return res.status(409).json({
-                    message: 'Mail exists'
-                });
-            } else {
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if (err) {
-                        return res.status(500).json({
-                            error: err
-                        });
-                    } else {
-                        const user = new User({
-                            _id: new mongoose.Types.ObjectId(),
-                            email: req.body.email,
-                            password: hash
+router.post('/signup', UserController.users_signup);
 
-                        });
-                        user
-                            .save()
-                            .then(result => {
-                                console.log(result);
-                                res.status(201).json({
-                                    message: 'User created'
-                                });
-                            })
-                            .catch(err => {
-                                console.log(err);
-                                res.status(500).json({
-                                    error: err
-                                });
-                            });
-                    }
-                });
-            }
-        })
-});
+router.post('/login', UserController.users_login);
 
-router.delete('/:userId', (req, res, next) => {
-    User.remove({ _id: req.params.userId})
-    .exec()
-    .then( result => {
-        res.status(200).json({
-            message: "User deleted"
-        })
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.delete('/:userId', checkAuth, UserController.users_delete_user);
 
 module.exports = router;
